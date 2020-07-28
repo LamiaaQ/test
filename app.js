53e676c2d00325e9e0a3e2de842b5d7b8af68ea2
LamiaaQ <68843047+LamiaaQ@users.noreply.github.com>
2020-07-28 16:43:12 +0300

    modify app.js to calculate % of files languages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const testFolder = './myFolder';
 const fs = require('fs');
+var path = require('path');
 
 fs.readdir(testFolder, (err, files) => {
     let javas=0;
@@ -9,9 +10,40 @@ fs.readdir(testFolder, (err, files) => {
     let css=0;
     let html=0;
     let py=0;
-    console.log(py);
+   // console.log(files.length); 
+    //console.log(py);
   files.forEach(file => {
-    console.log(file);
+   // console.log(file);
+   // console.log(path.extname(file));
+    if(path.extname(file)== '.html'){
+        html++;
+    }
+    if(path.extname(file)== '.js'){
+        javas++;
+    }
+    if(path.extname(file)== '.py'){
+        py++;
+    }
+    if(path.extname(file)== '.cpp'){
+        cpp++;
+    }
+    if(path.extname(file)== '.c'){
+        c++;
+    }
+    if(path.extname(file)== '.css'){
+        css++;
+    }
     
-  });
+   //var ar = file.split('.');
+});
+   // console.log(html);
+    //console.log(javas);
+    let sum = html+javas+css+py+cpp+c;
+    //console.log((html/sum)*100);
+    console.log(`html: ${(html/sum)*100}%`);
+    console.log(`css: ${(css/sum)*100}%`);
+    console.log(`javaScript: ${(javas/sum)*100}%`);
+    console.log(`python: ${(py/sum)*100}%`);
+    console.log(`c++: ${(cpp/sum)*100}%`);
+    console.log(`c: ${(c/sum)*100}%`);
 });
